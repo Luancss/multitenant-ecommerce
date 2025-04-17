75fa862e98ba06745ae56d5cd9ee8fdc15971525
Luan <ventobo123@gmail.com>
2025-04-16 22:38:44 -0300

    feat: add layout and navbar components for home page structure

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./navbar";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function ({ children }: Props) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
