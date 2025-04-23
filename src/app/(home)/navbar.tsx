81fcf0693e9c8b7260481f6c2a8f103582e22ab3
Luan <ventobo123@gmail.com>
2025-04-22 23:01:39 -0300

    feat: add About, Contact, Features, and Pricing pages with basic structure

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -72,6 +72,22 @@ export const Navbar = () => {
           </NavbarItem>
         ))}
       </div>
+
+      <div className="hidden lg:flex">
+        <Button
+          asChild
+          variant="secondary"
+          className="border-l border-t-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
+        >
+          <Link href="/sign-in">Log in</Link>
+        </Button>
+        <Button
+          asChild
+          className="border-l border-t-0 border-r-0 px-12 h-full rounded-none text-white bg-black hover:bg-pink-400 hover:text-black transition-colors text-lg"
+        >
+          <Link href="/sign-up">Start selling</Link>
+        </Button>
+      </div>
     </nav>
   );
 };
