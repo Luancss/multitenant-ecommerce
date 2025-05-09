89937a628e891b618125961fd39cdadd6642f0ec
Luan <ventobo123@gmail.com>
2025-05-08 23:52:17 -0300

    feat: implement SearchFilters component and integrate with layout for category filtering

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,33 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
 
 interface Props {
   children: React.ReactNode;
 }
 
-export default function ({ children }: Props) {
+export default async function ({ children }: Props) {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+    depth: 1, // Populate subscategories
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilters data={data} />
       <div className="flex-1">{children}</div>
       <Footer />
     </div>
