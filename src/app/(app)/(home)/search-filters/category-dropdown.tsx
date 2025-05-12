9641ed2a12eb48a5bce2ebfe8b5b3b1c779f0a27
Luan <ventobo123@gmail.com>
2025-05-12 00:02:21 -0300

    feat: implement CategoryDropdown component with hover functionality for subcategories

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useRef, useState } from "react";
 
 interface Props {
   category: any;
@@ -11,5 +15,45 @@ export const CategoryDropdown = ({
   isActive,
   isNavigationHovered,
 }: Props) => {
-  return <Button variant="link">{category.name}</Button>;
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const onMouseEnter = () => {
+    if (category.subcategories) {
+      setIsOpen(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div
+      ref={dropdownRef}
+      className="relative"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <div className={"relative"}>
+        <Button
+          variant="elevated"
+          className={cn(
+            "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+            isActive && !isNavigationHovered && "bg-white border-primary"
+          )}
+        >
+          {category.name}
+        </Button>
+        {category.subcategories && category.subcategories.length > 0 && (
+          <div
+            className={cn(
+              "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+              isOpen && "opacity-100"
+            )}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
